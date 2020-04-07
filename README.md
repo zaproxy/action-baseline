7125b8df3241a4d43dbae98a63c092dcf7906786
sshniro <sshniro@gmail.com>
2020-04-07 14:21:09 +0200

    Fixing review comments

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# action-baseline
-A GitHub Action for running the OWASP ZAP Baseline scan
+# Action Baseline
+
+A GitHub Action for running the OWASP ZAP [Baseline scan](https://github.com/zaproxy/zaproxy/wiki/ZAP-Baseline-Scan). 
+
+The ZAP baseline action scans a target URL for vulnerabilities and maintains an issue in GitHub repository for the
+identified alerts. Read the following ZAP blog for additional information.
+
+
+## Inputs
+
+### `Github token`
+
+**Required** ZAP action uses the default action token provided by GitHub to create and update the issue for the baseline scan.
+
+### `target`
+
+**Required** The URL of the web application to be scanned. This can be either a publicly available web application or a locally
+accessible URL.
+
+### `docker_name`
+
+**Optional** The name of the docker file to be executed. By default the action runs the stable version of ZAP. But you can 
+configure the parameter to use the weekly builds.
+
+### `rules_file_name`
+
+**Optional** You can also specify a relative path to the rules file to ignore any alerts from the ZAP scan. Make sure to create
+the rules file inside the relevant repository. The following shows a sample rules file configuration.
+
+```tsv
+10011	IGNORE	(Cookie Without Secure Flag)
+10015	IGNORE	(Incomplete or No Cache-control and Pragma HTTP Header Set)
+``` 
+
+## Example usage
+
+** Basic **
+```
+steps:
+  - name: ZAP Scan
+    uses: zap/action-baseline
+    with:
+      token: ${{ secrets.GIT_TOKEN }}
+      target: 'https://www.zaproxy.org/'
+```
+
+** Advanced **
+
+```
+on: [push]
+
+jobs:
+  zap_scan:
+    runs-on: ubuntu-latest
+    name: Scan the webapplication
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+        with:
+          ref: master
+      - name: ZAP Scan
+        uses: zap/action-baseline
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          docker_name: 'owasp/zap2docker-stable'
+          target: 'https://www.example.com'
+          rules_file_name: '.zap/rules.tsv'
+```
+
+## Additional Information
+
+The following image shows how the alerts will be updated for consequent scan.
+
+- First Comment: A fresh ZAP scan creates a new issue with links to the report.
+- Second Comment: During the second scan we have ignored two alerts via the rule configurations. Therefore, ZAP ignores the two alerts and reports as the two alerts have been resolved.
+- Third Comment: During the third scan we are ignoring all the alerts. If all the alerts have been resolved, then the action will automatically close the pending issue. 
+
+
+![issue](./issue.png)
---
+++ action-helper.js
@@ -0,0 +1,242 @@
+const fs = require('fs');
+const _ = require('lodash');
+const readline = require('readline');
+const AdmZip = require('adm-zip');
+const request = require('request');
+const artifact = require('@actions/artifact');
+
+function createReadStreamSafe(filename, options) {
+    return new Promise((resolve, reject) => {
+        const fileStream = fs.createReadStream(filename, options);
+        fileStream.on('error', reject).on('open', () => {
+            resolve(fileStream);
+        });
+    });
+}
+
+let actionHelper = {
+
+    getRunnerID: ((body) => {
+        let results = body.match('RunnerID:\\d+');
+        if (results !== null && results.length !== 0) {
+            return results[0].split(':')[1];
+        }
+        return null;
+    }),
+
+    processLineByLine: (async (tsvFile) => {
+        let plugins = [];
+        try {
+            const fileStream = await createReadStreamSafe(tsvFile);
+            const rl = readline.createInterface({
+                input: fileStream,
+                crlfDelay: Infinity
+            });
+            for await (const line of rl) {
+                if (line.charAt(0) !== '#') {
+                    let tmp = line.split('\t');
+                    if (tmp[0].trim() !== '' && tmp[1].trim().toUpperCase() === 'IGNORE') {
+                        plugins.push(tmp[0].trim());
+                    }
+                }
+            }
+        } catch (err) {
+            console.log(`Error when reading the rules file: ${tsvFile}`)
+        }
+
+        return plugins;
+    }),
+
+    createMessage: ((sites, runnerID, runnerLink) => {
+        const NXT_LINE = '\n';
+        const TAB = "\t";
+        const BULLET = "-";
+        let msg = '';
+
+        sites.forEach((site => {
+            msg = msg + `${BULLET} Site[${site["@name"]}] ${NXT_LINE}`;
+            if (site.hasOwnProperty('alerts')) {
+                if (site.alerts.length !== 0) {
+                    msg = `${msg} ${TAB} **New Alerts** ${NXT_LINE}`;
+                    site.alerts.forEach((alert) => {
+                        msg = msg + TAB + `${BULLET} Alert[${alert.pluginid}] count(${alert.instances.length}): ${alert.name} ${NXT_LINE}`
+                    });
+                    msg = msg + NXT_LINE
+                }
+            }
+
+            if (site.hasOwnProperty('removedAlerts')) {
+                if (site.removedAlerts.length !== 0) {
+                    msg = `${msg} ${TAB} **Resolved Alerts** ${NXT_LINE}`;
+                    site.removedAlerts.forEach((alert) => {
+                        msg = msg + TAB + `${BULLET} Alert[${alert.pluginid}] count(${alert.instances.length}): ${alert.name} ${NXT_LINE}`
+                    });
+                    msg = msg + NXT_LINE
+                }
+            }
+
+            if (site.hasOwnProperty('ignoredAlerts')) {
+                if (site.ignoredAlerts.length !== 0) {
+                    msg = `${msg} ${TAB} **Ignored Alerts** ${NXT_LINE}`;
+                    site.ignoredAlerts.forEach((alert) => {
+                        msg = msg + TAB + `${BULLET} Alert[${alert.pluginid}] count(${alert.instances.length}): ${alert.name} ${NXT_LINE}`
+                    });
+                    msg = msg + NXT_LINE
+                }
+            }
+
+            msg = msg + NXT_LINE
+        }));
+        if (msg.trim() !== '') {
+            msg = msg + NXT_LINE + runnerLink;
+            msg = msg + NXT_LINE + runnerID;
+        }
+        return msg
+    }),
+
+    generateDifference: ((newReport, oldReport) => {
+        newReport.updated = false;
+        let siteClone = [];
+        newReport.site.forEach((newReportSite) => {
+            // Check if the new report site already exists in the previous report
+            let previousSite = _.filter(oldReport.site, s => s['@name'] === newReportSite['@name']);
+            // If does not exists add it to the array without further processing
+            if (previousSite.length === 0) {
+                newReport.updated = true;
+                siteClone.push(newReportSite);
+            } else {
+                // deep clone the variable for further processing
+                let newSite = JSON.parse(JSON.stringify(newReportSite));
+                let currentAlerts = newReportSite.alerts;
+                let previousAlerts = previousSite[0].alerts;
+
+                let newAlerts = _.differenceBy(currentAlerts, previousAlerts, 'pluginid');
+                let removedAlerts = _.differenceBy(previousAlerts, currentAlerts, 'pluginid');
+
+                let ignoredAlerts = [];
+                if (newReportSite.hasOwnProperty('ignoredAlerts') && previousSite[0].hasOwnProperty('ignoredAlerts')) {
+                    ignoredAlerts = _.differenceBy(newReportSite['ignoredAlerts'], previousSite[0]['ignoredAlerts'], 'pluginid');
+                }else if(newReportSite.hasOwnProperty('ignoredAlerts')){
+                    ignoredAlerts = newReportSite['ignoredAlerts']
+                }
+
+                removedAlerts = _.differenceBy(removedAlerts, ignoredAlerts, 'pluginid');
+
+                newSite.alerts = newAlerts;
+                newSite.removedAlerts = removedAlerts;
+                newSite.ignoredAlerts = ignoredAlerts;
+                siteClone.push(newSite);
+
+                if (newAlerts.length !== 0 || removedAlerts.length !== 0 || ignoredAlerts.length !== 0) {
+                    newReport.updated = true;
+                }
+            }
+        });
+        return siteClone;
+    }),
+
+    readMDFile: (async (reportName) => {
+        let res = '';
+        try {
+            res = fs.readFileSync(reportName, {encoding: 'base64'});
+        } catch (err) {
+            console.log('error while reading the markdown file!')
+        }
+        return res;
+    }),
+
+    checkIfAlertsExists: ((jsonReport) => {
+        return jsonReport.site.some((s) => {
+            return (s.hasOwnProperty('alerts') && s.alerts.length !== 0);
+        });
+    }),
+
+
+    filterReport: (async (jsonReport, plugins) => {
+        jsonReport.site.forEach((s) => {
+            if (s.hasOwnProperty('alerts') && s.alerts.length !== 0) {
+                console.log(`starting to filter the alerts for site: ${s['@name']}`);
+                let newAlerts = s.alerts.filter(function (e) {
+                    return !plugins.includes(e.pluginid)
+                });
+                let removedAlerts = s.alerts.filter(function (e) {
+                    return plugins.includes(e.pluginid)
+                });
+                s.alerts = newAlerts;
+                s.ignoredAlerts = removedAlerts;
+
+                console.log(`#${newAlerts.length} alerts have been identified` +
+                    ` and #${removedAlerts.length} alerts have been ignored for the site.`);
+            }
+        });
+        return jsonReport;
+    }),
+
+
+    readPreviousReport: (async (octokit, owner, repo, workSpace, runnerID) => {
+        let previousReport;
+        try{
+            let artifactList = await octokit.actions.listWorkflowRunArtifacts({
+                owner: owner,
+                repo: repo,
+                run_id: runnerID
+            });
+
+            let artifacts = artifactList.data.artifacts;
+            let artifactID;
+            if (artifacts.length !== 0) {
+                artifacts.forEach((a => {
+                    if (a['name'] === 'zap_scan') {
+                        artifactID = a['id']
+                    }
+                }));
+            }
+
+            if (artifactID !== undefined) {
+                let download = await octokit.actions.downloadArtifact({
+                    owner: owner,
+                    repo: repo,
+                    artifact_id: artifactID,
+                    archive_format: 'zip'
+                });
+
+                await new Promise(resolve =>
+                    request(download.url)
+                        .pipe(fs.createWriteStream(`${workSpace}/zap_scan.zip`))
+                        .on('finish', () => {
+                            resolve();
+                        }));
+
+                let zip = new AdmZip(`${workSpace}/zap_scan.zip`);
+                let zipEntries = zip.getEntries();
+
+                await zipEntries.forEach(function (zipEntry) {
+                    if (zipEntry.entryName === "report_json.json") {
+                        previousReport = JSON.parse(zipEntry.getData().toString('utf8'));
+                    }
+                });
+            }
+        }catch (e) {
+            console.log(`Error occurred while downloading the artifacts!`)
+        }
+        return previousReport;
+    }),
+
+    uploadArtifacts: (async (rootDir, mdReport, jsonReport) => {
+        const artifactClient = artifact.create()
+        const artifactName = 'zap_scan';
+        const files = [
+            `${rootDir}/${mdReport}`,
+            `${rootDir}/${jsonReport}`,
+        ];
+        const rootDirectory = rootDir;
+        const options = {
+            continueOnError: true
+        };
+
+        await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+    })
+
+};
+
+module.exports = actionHelper;
