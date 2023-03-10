74055280e4df57930e4936405789062bed092aa1
atilsensalduz <atil.eren@outlook.com>
2023-03-10 12:09:16 +0000

    revert: formatting
    Signed-off-by: atilsensalduz <atil.eren@outlook.com>

--- index.js
+++ index.js
@@ -1,106 +1,75 @@
-const core = require("@actions/core");
-const exec = require("@actions/exec");
-const common = require("@zaproxy/actions-common-scans");
-const _ = require("lodash");
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const common = require('@zaproxy/actions-common-scans');
+const _ = require('lodash');
 
 async function run() {
-  try {
-    let workspace = process.env.GITHUB_WORKSPACE;
-    let currentRunnerID = process.env.GITHUB_RUN_ID;
-    let repoName = process.env.GITHUB_REPOSITORY;
-    let token = core.getInput("token");
-    let docker_name = core.getInput("docker_name");
-    let target = core.getInput("target");
-    let rulesFileLocation = core.getInput("rules_file_name");
-    let cmdOptions = core.getInput("cmd_options");
-    let issueTitle = core.getInput("issue_title");
-    let failAction = core.getInput("fail_action");
-    let allowIssueWriting = core.getInput("allow_issue_writing");
-    let createIssue = true;
-    let jsonReportName = core.getInput("json_report_name");
-    let mdReportName = core.getInput("md_report_name");
-    let htmlReportName = core.getInput("html_report_name");
-
-    if (
-      !(
-        String(failAction).toLowerCase() === "true" ||
-        String(failAction).toLowerCase() === "false"
-      )
-    ) {
-      console.log(
-        "[WARNING]: 'fail_action' action input should be either 'true' or 'false'"
-      );
-    }
-
-    if (String(allowIssueWriting).toLowerCase() === "false") {
-      createIssue = false;
-    }
-
-    console.log("starting the program");
-    console.log("github run id :" + currentRunnerID);
-
-    let plugins = [];
-    if (rulesFileLocation) {
-      plugins = await common.helper.processLineByLine(
-        `${workspace}/${rulesFileLocation}`
-      );
-    }
-
-    // Create the files so we can change the perms and allow the docker non root user to update them
-    await exec.exec(
-      `touch ${jsonReportName} ${mdReportName} ${htmlReportName}`
-    );
-    await exec.exec(
-      `chmod a+w ${jsonReportName} ${mdReportName} ${htmlReportName}`
-    );
-
-    await exec.exec(`docker pull ${docker_name} -q`);
-    let command =
-      `docker run -v ${workspace}:/zap/wrk/:rw --network="host" ` +
-      `-t ${docker_name} zap-baseline.py -t ${target} -J ${jsonReportName} -w ${mdReportName}  -r ${htmlReportName} ${cmdOptions}`;
-
-    if (plugins.length !== 0) {
-      command = command + ` -c ${rulesFileLocation}`;
-    }
 
     try {
-      await exec.exec(command);
-    } catch (err) {
-      if (err.toString().includes("exit code 3")) {
-        core.setFailed("failed to scan the target: " + err.toString());
-        return;
-      }
+        let workspace = process.env.GITHUB_WORKSPACE;
+        let currentRunnerID = process.env.GITHUB_RUN_ID;
+        let repoName = process.env.GITHUB_REPOSITORY;
+        let token = core.getInput('token');
+        let docker_name = core.getInput('docker_name');
+        let target = core.getInput('target');
+        let rulesFileLocation = core.getInput('rules_file_name');
+        let cmdOptions = core.getInput('cmd_options');
+        let issueTitle = core.getInput('issue_title');
+        let failAction = core.getInput('fail_action');
+        let allowIssueWriting = core.getInput('allow_issue_writing');
+        let createIssue = true;
+        let jsonReportName = core.getInput("json_report_name");
+        let mdReportName = core.getInput("md_report_name");
+        let htmlReportName = core.getInput("html_report_name");
 
-      if (
-        (err.toString().includes("exit code 2") ||
-          err.toString().includes("exit code 1")) &&
-        String(failAction).toLowerCase() === "true"
-      ) {
-        console.log(
-          `[info] By default ZAP Docker container will fail if it identifies any alerts during the scan!`
-        );
-        core.setFailed(
-          "Scan action failed as ZAP has identified alerts, starting to analyze the results. " +
-            err.toString()
-        );
-      } else {
-        console.log(
-          "Scanning process completed, starting to analyze the results!"
-        );
-      }
+        if (!(String(failAction).toLowerCase() === 'true' || String(failAction).toLowerCase() === 'false')) {
+            console.log('[WARNING]: \'fail_action\' action input should be either \'true\' or \'false\'');
+        }
+
+        if (String(allowIssueWriting).toLowerCase() === 'false') {
+            createIssue = false;
+        }
+
+        console.log('starting the program');
+        console.log('github run id :' + currentRunnerID);
+
+        let plugins = [];
+        if (rulesFileLocation) {
+            plugins = await common.helper.processLineByLine(`${workspace}/${rulesFileLocation}`);
+        }
+
+        // Create the files so we can change the perms and allow the docker non root user to update them
+        await exec.exec(`touch ${jsonReportName} ${mdReportName} ${htmlReportName}`);
+        await exec.exec(`chmod a+w ${jsonReportName} ${mdReportName} ${htmlReportName}`);
+
+        await exec.exec(`docker pull ${docker_name} -q`);
+        let command = (`docker run -v ${workspace}:/zap/wrk/:rw --network="host" ` +
+            `-t ${docker_name} zap-baseline.py -t ${target} -J ${jsonReportName} -w ${mdReportName}  -r ${htmlReportName} ${cmdOptions}`);
+
+        if (plugins.length !== 0) {
+            command = command + ` -c ${rulesFileLocation}`
+        }
+
+        try {
+            await exec.exec(command);
+        } catch (err) {
+            if (err.toString().includes('exit code 3')) {
+                core.setFailed('failed to scan the target: ' + err.toString());
+                return
+            }
+
+            if ((err.toString().includes('exit code 2') || err.toString().includes('exit code 1'))
+                    && String(failAction).toLowerCase() === 'true') {
+                console.log(`[info] By default ZAP Docker container will fail if it identifies any alerts during the scan!`);
+                core.setFailed('Scan action failed as ZAP has identified alerts, starting to analyze the results. ' + err.toString());
+            }else {
+                console.log('Scanning process completed, starting to analyze the results!')
+            }
+        }
+        await common.main.processReport(token, workspace, plugins, currentRunnerID, issueTitle, repoName, createIssue);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    await common.main.processReport(
-      token,
-      workspace,
-      plugins,
-      currentRunnerID,
-      issueTitle,
-      repoName,
-      createIssue
-    );
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 run();
